f84ffb6fcbb0fcdf341057e1387759b797b40c5a
dral123456 <marcelinoarldrich@gmail.com>
2026-03-19 09:59:08 +0800

    sidebar and navbar, work on logout

--- views/modules/sample.php
+++ views/modules/sample.php
@@ -1,19 +1,16 @@
 <body>
+<?php 
+    include 'views/partials/header.php'; 
+    include 'views/partials/sidebar.php';
+?>
 <!-- START -->
 <div class="position-fixed top-0 bottom-0 end-0 start-0 z-0 bg-pattern"></div>
 <div class="auth-pattern-shapes d-none d-lg-block"></div>
 <div class="auth-pattern-outline d-none d-lg-block"></div>
 <div class="auth-pattern-shape extra d-none d-lg-block"></div>
 <div class="auth-pattern-extra d-none d-lg-block"></div><header class="px-3 px-md-8 py-5 position-absolute top-0 d-flex justify-content-between align-items-center w-100 z-1">
-  <a href="index.html" class="d-flex align-items-end logo-main">
-    <img height="35" class="logo-dark" alt="Dark Logo" src="views/assets/images/logo-md.png">
-    <h3 class="text-body-emphasis fw-bolder mb-0 ms-1">Urbix</h3>
-  </a>
-  <ul class="list-inline mb-0">
-    <li class="list-inline-item pe-4 border-end"><a href="index.html" class="link-body-emphasis">Home</a></li>
-    <li class="list-inline-item pe-4 border-end"><a href="#!" class="link-body-emphasis">Support</a></li>
-    <li class="list-inline-item"><a href="#!" class="link-body-emphasis">Documentation</a></li>
-  </ul>
+  
+  
 </header>
 <div class="container">
   <div class="row justify-content-center align-items-center min-vh-100 pt-20 pb-10">
